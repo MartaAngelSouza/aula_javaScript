92bdbfea13bd1c388859f2044d3839552cbe3015
Marta Silva <martaangelsilva@hotmail.com>
2021-03-15 17:58:06 -0300

    inclusão

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+
+var d = new Date();
+alert(d);
+
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+/*estrutura de repetição*/
+// var count =0;
+// while(count<= 5){
+//     console.log(count);
+//     // count =count + 1;
+//     count ++; /*recebe ele mais um*/
+// };
+
+
+// var idade = prompt("Qual sua idade"); /*faz uma pergunta para o usuario*/
+// // var idade = 18;
+// if(idade >= 18){
+//     alert("maior de idade");
+// }else{
+//     alert("menor de idade")
+//     };
+
+// var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}] /*conceito de json dicionario*/
+// console.log(frutas);
+// alert(frutas[1].nome); 
+
+
+// var fruta = {nome:"maça", cor:"vermelha"} /*conceito de json dicionario*/
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+
+// var lista =["maça", "pêra","laranja"];
+// lista.push("uva"); /*incluir um item na lista*/
+// lista.pop(); /*retira item da lista*/
+// console.log(lista); /*mostrar uma lista*/
+// console.log(lista[1]); /*mostrar um item da lista, sempre inicia com zero */
+// console.log(lista.length); /*verifica quantidade de item dentro da lista*/
+// console.log(lista.reverse()); /*mostra quantidade de itens e quais são os itens*/
+// console.log(lista.toString()); /*tira o array e muda para string*/
+// console.log(lista.join(" - ")); /*tira o array e muda para string inclui o que vc quiser no lugar da virgula*/
+
+
+// var nome ="Marta da Silva";
+// var idade = 50;
+// var idade2 = 10;
+// var frase = "Japão é o melhor time"
+// alert(nome + " tem " + idade + " anos");
+// alert (idade + idade2);
+// console.log(nome);
+// console.log(idade + idade2);
+// console.log (frase.toLowerCase());
+// alert(frase.replace("Japão", "Brasil"));
